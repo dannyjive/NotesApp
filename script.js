3e05895de7e40bed1d270bee5f0edc2f8023ca78
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2026-06-29 14:34:16 -0700

    addes search

--- script.js
+++ script.js
@@ -3,46 +3,83 @@ const noteInput = document.getElementById("notesText");
 const addBtn = document.getElementById("addBtn");
 const notesList = document.getElementById("notesList");
 const selectedNote = document.getElementById("selectedNote");
+const searchInput = document.getElementById("searchInput");
 
 const notes = [];
 let selectedId = null;
+let searchText = "";
 
-function renderNotes(){
+function renderNotes() {
     notesList.innerHTML = "";
+    
+    const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(searchText))
 
-    notes.forEach(note => {
+    filteredNotes.forEach((note) => {
         const div = document.createElement("div");
-        div.textContent = note.title
-
+        div.textContent = note.title;
+        
+        
+        
         div.addEventListener("click", () => {
-            selectedId = note.id
+            selectedId = note.id;
+            
+            renderNotes();
             renderSelectedNote();
         });
-
+        
+        if (note.id === selectedId) {        
+            div.classList.add('active');
+        }
+        
         notesList.appendChild(div);
     });
 }
 
-function renderSelectedNote(){
-    
+searchInput.addEventListener("input", () => {
+    searchText = searchInput.value.toLowerCase();
+
+    renderNotes()
+})
+
+function renderSelectedNote() {
+  selectedNote.innerHTML = "";
+
+  const div = document.createElement("div");
+  const headline = document.createElement("h2");
+  const para = document.createElement("p");
+
+  const currentNote = notes.find((note) => note.id === selectedId);
+  
+  if (!currentNote) return;
+  
+  headline.textContent = currentNote.title;
+  para.textContent = currentNote.text
+
+  div.appendChild(headline);
+  div.appendChild(para);
+  selectedNote.appendChild(div);
+
 }
 
 addBtn.addEventListener("click", () => {
   const title = titleInput.value.trim();
   const text = noteInput.value.trim();
 
-    if (!title.trim()) return;
+  if (!title.trim()) return;
 
-    const note = {
-        id: Date.now(),
-        title,
-        text
-    };
+  const note = {
+    id: Date.now(),
+    title,
+    text,
+  };
 
-    notes.push(note);
-    
-    renderNotes();
+  notes.push(note);
 
-    titleInput.value = "";
-    noteInput.value = "";
+  selectedId = note.id;
+
+  renderNotes();
+  renderSelectedNote();
+
+  titleInput.value = "";
+  noteInput.value = "";
 });
