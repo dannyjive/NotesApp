d71f6321720606f9ee1f43146ec4010a7c6b2dea
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2026-06-29 21:40:46 -0700

    added search and delete

--- script.js
+++ script.js
@@ -4,42 +4,55 @@ const addBtn = document.getElementById("addBtn");
 const notesList = document.getElementById("notesList");
 const selectedNote = document.getElementById("selectedNote");
 const searchInput = document.getElementById("searchInput");
+const delBtn = document.getElementById("delBtn");
 
-const notes = [];
+let notes = [];
 let selectedId = null;
 let searchText = "";
 
 function renderNotes() {
-    notesList.innerHTML = "";
-    
-    const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(searchText))
+  notesList.innerHTML = "";
 
-    filteredNotes.forEach((note) => {
-        const div = document.createElement("div");
-        div.textContent = note.title;
-        
-        
-        
-        div.addEventListener("click", () => {
-            selectedId = note.id;
-            
-            renderNotes();
-            renderSelectedNote();
-        });
-        
-        if (note.id === selectedId) {        
-            div.classList.add('active');
-        }
-        
-        notesList.appendChild(div);
+  const filteredNotes = notes.filter((note) =>
+    note.title.toLowerCase().includes(searchText),
+  );
+
+  filteredNotes.forEach((note) => {
+    const div = document.createElement("div");
+    div.textContent = note.title;
+
+    div.addEventListener("click", () => {
+      selectedId = note.id;
+
+      renderNotes();
+      renderSelectedNote();
     });
+
+    if (note.id === selectedId) {
+      div.classList.add("active");
+    }
+
+    notesList.appendChild(div);
+  });
 }
 
 searchInput.addEventListener("input", () => {
-    searchText = searchInput.value.toLowerCase();
+  searchText = searchInput.value.toLowerCase();
 
-    renderNotes()
-})
+  renderNotes();
+});
+
+delBtn.addEventListener("click", () => {
+  if (!selectedId) return;
+
+    notes = notes.filter((note) => note.id !== selectedId);
+
+    selectedId = null;
+
+    renderNotes();
+    renderSelectedNote();
+
+});
 
 function renderSelectedNote() {
   selectedNote.innerHTML = "";
@@ -49,16 +62,15 @@ function renderSelectedNote() {
   const para = document.createElement("p");
 
   const currentNote = notes.find((note) => note.id === selectedId);
-  
+
   if (!currentNote) return;
-  
+
   headline.textContent = currentNote.title;
-  para.textContent = currentNote.text
+  para.textContent = currentNote.text;
 
   div.appendChild(headline);
   div.appendChild(para);
   selectedNote.appendChild(div);
-
 }
 
 addBtn.addEventListener("click", () => {
